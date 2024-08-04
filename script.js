d1e8e2c8feb7d296b58edfa2736294f1ae629555
Bauyrzhan <kzeq1995@gmail.com>
2024-08-04 14:58:14 -0700

    Added more info

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ console.log(
     numOfKids +
     ' kids. ',
 )
+numOfKids = prompt('Enter Number Of Kids')
+job = prompt('Enter Your Job Title')
+city = prompt('Enter The City')
+spouse = prompt("Enter Spouse's Name ")
+
+alert(
+  'You will be a ' +
+    job +
+    ' in ' +
+    city +
+    ' , ' +
+    ' and married to ' +
+    spouse +
+    ' with' +
+    numOfKids +
+    ' kids. ',
+)
