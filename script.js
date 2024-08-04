3fbcab9feab8d3c8579de3f4c8d83b0704c71c4e
Bauyrzhan <kzeq1995@gmail.com>
2024-08-04 14:49:08 -0700

    added a fortune teller via function

---
+++ script.js
@@ -0,0 +1,17 @@
+var numOfKids = 3
+var job = 'Software Engineer'
+var city = 'Vancouver'
+var spouse = 'Julie'
+
+console.log(
+  'You will be a ' +
+    job +
+    ' in ' +
+    city +
+    ' , ' +
+    ' and married to ' +
+    spouse +
+    ' with' +
+    numOfKids +
+    ' kids. ',
+)
